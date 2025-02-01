d28950feabf0bfc01287a2a089f6edc605776d01
Thanus Kumaar <thanuskumaara@gmail.com>
2025-02-01 17:32:25 +0530

    created https server for secure communication with the client

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const { json } = require("body-parser");
-const express = require("express");
-const cors = require("cors");
-require('dotenv').config()
+import express from "express";
+import https from "https";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import fs from "fs";
+
+dotenv.config();
 
 const app = express();
-app.use(json());
+
+app.use(bodyParser.json());
 app.use(cors());
+
+const options = {
+  key: fs.readFileSync("certificate/server.key"), // Use your key file
+  cert: fs.readFileSync("certificate/server.cert"), // Use your cert file
+};
 
 app.get("/test", (req, res) => {
   res.status(200).json({ MSG: "Server is runnning :)" });
 });
 
-app.listen(process.env.SERVER_PORT, (err) => {
-  if (err) console.log("Unable to start server! \nError occured: ", err);
-  else console.log("Server listening at PORT: ", process.env.SERVER_PORT);
+https.createServer(options, app).listen(443, () => {
+  console.log("HTTPS Server running on https://localhost:443");
+});
+
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers.host + req.url });
+  res.end();
+}).listen(80, () => {
+  console.log("HTTP Server running on http://localhost:80 (Redirects to HTTPS)");
 });
