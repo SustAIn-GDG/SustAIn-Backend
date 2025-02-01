88c778cb73a954e7e1733a7ed8a9369b192ab453
Thanus Kumaar <thanuskumaara@gmail.com>
2025-02-01 16:59:30 +0530

    initial route for testing is created.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const { json } = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
 
 const app = express();
+app.use(json());
+app.use(cors());
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ MSG: "Server is runnning :)" });
+});
 
 app.listen(process.env.SERVER_PORT, (err) => {
-  console.log("Unable to start server! \nError occured: ", err);
+  if (err) console.log("Unable to start server! \nError occured: ", err);
+  else console.log("Server listening at PORT: ", process.env.SERVER_PORT);
 });
