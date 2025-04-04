f0718f227b78da0e7f39ff04069cebc0fa8c70e7
Thanus Kumaar <thanuskumaara@gmail.com>
2025-04-05 01:36:16 +0530

    Increased json parsing limit

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 const corsOptions = {
   origin: function (origin, callback) {
